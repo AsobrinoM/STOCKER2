5f086066726d4445642467ceb73bd911e139e8cf
AsobrinoM <asobrinom16@educastillalamancha.com>
2023-11-01 12:24:26 +0100

    Ingreso y actualizacion
    COMPLETADO AAAAAAAAA

--- app/src/main/java/com/example/stocker2/ActivityIngresoProductos.kt
+++ app/src/main/java/com/example/stocker2/ActivityIngresoProductos.kt
@@ -3,11 +3,14 @@ package com.example.stocker2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.stocker2.databinding.ActivityIngresoProductosBinding
 import com.example.stocker2.databinding.LayoutRegistroBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import java.util.Objects
 
 private lateinit var binding: ActivityIngresoProductosBinding
 class ActivityIngresoProductos : AppCompatActivity() {
@@ -20,7 +23,9 @@ class ActivityIngresoProductos : AppCompatActivity() {
         var datosEnviados=objIntent.getStringExtra("NombreEmpresa")
         binding.btnGuardarProducto.setOnClickListener{
 
+            if (datosEnviados != null) {
                 guardarRegistro(datosEnviados)
+            }
 
         }
     }
@@ -28,41 +33,36 @@ class ActivityIngresoProductos : AppCompatActivity() {
         binding=ActivityIngresoProductosBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-    private fun guardarRegistro(NombreEmpresa:String?){
-        myCollection.document(binding.etNombProd.text.toString()).get()
+    private fun guardarRegistro(NombreEmpresa:String){
+           myCollection.document(NombreEmpresa).get()
             .addOnSuccessListener {
-                if (it.exists()){
-                    myCollection.document(binding.etNombProd.text.toString()).set(
-                        hashMapOf(
-                            "cantidad" to (binding.etCantProd.text.toString().toInt()+it.get("cantidad").toString().toInt()),
-                            "Supermercado" to (NombreEmpresa).toString()
-                        )
-                    )
-                    resultadoOperacion("Se ha añadido la cantidad correctamente")
+                    if (it.contains(binding.etNombProd.text.toString())){
+                        myCollection.document(NombreEmpresa).set(
+                            hashMapOf(
+                                binding.etNombProd.text.toString() to (binding.etCantProd.text.toString().toInt()+it.get(binding.etNombProd.text.toString()).toString().toInt())
+                            ), SetOptions.merge())
+                            .addOnSuccessListener {
+                                resultadoOperacion("Se ha añadido la cantidad Correspondiente")
+                            }
+                            .addOnFailureListener{
+                                    e->
+                                Log.e("Firebase Update Error",e.message,e)
+                            }
+                    }
+                    else{
+                        myCollection.document(NombreEmpresa).set(
+                            hashMapOf(
+                                binding.etNombProd.text.toString() to (binding.etCantProd.text.toString().toInt())
+                            ), SetOptions.merge())
+                            .addOnSuccessListener {
+                                resultadoOperacion("El producto se ha registrado correctamente")
+                            }
+                            .addOnFailureListener{
+                                    e->
+                                Log.e("Firebase Update Error",e.message,e)
+                    }
                 }
-                else{
-                    myCollection.document(binding.etNombProd.text.toString()).set(
-                        hashMapOf(
-                            "cantidad" to (binding.etCantProd.text.toString().toInt()),
-                            "Supermercado" to (NombreEmpresa).toString()
-
-                        )
-
-
-                    )
-                    resultadoOperacion("El producto se ha registrado correctamente")
-
-                }
-            }
-
-
-
-
-
-
-
-
-    }
+    }}
     private fun resultadoOperacion(mensaje:String){
         binding.etCantProd.setText("")
         binding.etNombProd.setText("")
